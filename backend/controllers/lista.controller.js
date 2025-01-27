10f1c77b4dd54af69598be1593d7746a13293982
EleArtee <100388234+EleArtee@users.noreply.github.com>
2025-01-27 02:17:31 +0000

    edit: Updated Controllers (lista, review)

--- backend/controllers/lista.controller.js
+++ backend/controllers/lista.controller.js
@@ -1,115 +1,113 @@
 const db = require("../models");
-const Usuario = db.usuario;
+const   Lista = db.lista;
 const Op = db.Sequelize.Op;
 
 exports.create = (req, res) =>{
-    const usuario = {
-       nick: req.body.nick,
-       email: req.body.email,
-       contraseña: req.body.contraseña,
-       empresa: req.body.empresa,
+    const lista = {
+       titulo: req.body.titulo,
+       descripcion: req.body.descripcion,
     }
 
-    if (!req.body.nick){
+    if (!req.body.titulo){
         res.status(400).send({
             message: "Falta información obligatoria"
         });
         return;
     }
 
-    Usuario.create(usuario)
+    Lista.create(lista)
     .then(data =>{
         res.send(data);
     })
     .catch(err => {
         res.status(500).send({
             message:
-                err.message || "Algo pasó mientras se creaba el usuario de mesa."
+                err.message || "Algo pasó mientras se creaba el lista de mesa."
         })
     });
 };
 
 exports.findAll = (req, res) =>{
-    Usuario.findAll()
+    Lista.findAll()
     .then(data => {
         res.send(data);
     })
     .catch(err =>{
         res.status(500).send({
             message:
-                err.message || "Algún error ocurrió mientras se obtenían los usuarios de mesa."
+                err.message || "Algún error ocurrió mientras se obtenían los listas de mesa."
         })
     })
 
 };
 
 exports.findOne = (req, res) =>{
-    const id = req.params.idUser;
-    Usuario.findByPK(id)
+    const id = req.params.idList;
+    Lista.findByPK(id)
         .then(data =>{
             if (data) {
                 res.send(data);
             }
             else {
                 res.status(404).send({
-                    message: `No se pudo encontrar el usuario con el id=${id}.`
+                    message: `No se pudo encontrar el lista con el id=${id}.`
                 });
             }
         })
         .catch(err =>{
             res.status(500).send({
                 message:
-                    err.message || "Error obteniendo el usuario con el id =" + id
+                    err.message || "Error obteniendo el lista con el id =" + id
             })
         })
 };
 
 exports.update = (req, res) => {
-    const id = req.params.idUser;
+    const id = req.params.idList;
 
-    Usuario.update(req.body, {
+    Lista.update(req.body, {
         where: { id: id }
     })
         .then(num =>{
             if (num == 1) {
                 res.send({
-                    message: "El usuario fue actualizado exitosamente."
+                    message: "El lista fue actualizado exitosamente."
                 });
             } else {
                 res.send({
-                    message: `No se pudo actualizar el usuario con el id=${id}`
+                    message: `No se pudo actualizar el lista con el id=${id}`
                 });
             }
         })
         .catch(err =>{
             res.status(500).send({
                 message:
-                    err.message || "Error actualizando el usuario con el id =" + id
+                    err.message || "Error actualizando el lista con el id =" + id
             })
         })
 };
 
 exports.delete = (req, res) =>{
-    const id = req.params.idUser;
+    const id = req.params.idList;
 
-    Usuario.destroy( {
+    Lista.destroy( {
         where: { id: id },
     })
         .then(num =>{
             if (num == 1) {
                 res.send({
-                    message: "El usuario fue eliminado exitosamente."
+                    message: "El lista fue eliminado exitosamente."
                 });
             } else {
                 res.send({
-                    message: `No se pudo eliminar el usuario con el id=${id}`
+                    message: `No se pudo eliminar el lista con el id=${id}`
                 });
             }
         })
         .catch(err =>{
             res.status(500).send({
                 message:
-                    err.message || "Error eliminando el usuario con el id =" + id
+                    err.message || "Error eliminando el lista con el id =" + id
             })
         })
 
--- backend/controllers/review.controller.js
+++ backend/controllers/review.controller.js
@@ -1,115 +1,113 @@
 const db = require("../models");
-const Usuario = db.usuario;
+const Review = db.review;
 const Op = db.Sequelize.Op;
 
 exports.create = (req, res) =>{
-    const usuario = {
-       nick: req.body.nick,
-       email: req.body.email,
-       contraseña: req.body.contraseña,
-       empresa: req.body.empresa,
+    const review = {
+       estrellas: req.body.estrellas,
+       cuerpo: req.body.cuerpo,
     }
 
-    if (!req.body.nick){
+    if (!req.body.estrellas){
         res.status(400).send({
             message: "Falta información obligatoria"
         });
         return;
     }
 
-    Usuario.create(usuario)
+    Review.create(review)
     .then(data =>{
         res.send(data);
     })
     .catch(err => {
         res.status(500).send({
             message:
-                err.message || "Algo pasó mientras se creaba el usuario de mesa."
+                err.message || "Algo pasó mientras se creaba el review de mesa."
         })
     });
 };
 
 exports.findAll = (req, res) =>{
-    Usuario.findAll()
+    Review.findAll()
     .then(data => {
         res.send(data);
     })
     .catch(err =>{
         res.status(500).send({
             message:
-                err.message || "Algún error ocurrió mientras se obtenían los usuarios de mesa."
+                err.message || "Algún error ocurrió mientras se obtenían los reviews de mesa."
         })
     })
 
 };
 
 exports.findOne = (req, res) =>{
-    const id = req.params.idUser;
-    Usuario.findByPK(id)
+    const id = req.params.idReview;
+    Review.findByPK(id)
         .then(data =>{
             if (data) {
                 res.send(data);
             }
             else {
                 res.status(404).send({
-                    message: `No se pudo encontrar el usuario con el id=${id}.`
+                    message: `No se pudo encontrar el review con el id=${id}.`
                 });
             }
         })
         .catch(err =>{
             res.status(500).send({
                 message:
-                    err.message || "Error obteniendo el usuario con el id =" + id
+                    err.message || "Error obteniendo el review con el id =" + id
             })
         })
 };
 
 exports.update = (req, res) => {
-    const id = req.params.idUser;
+    const id = req.params.idReview;
 
-    Usuario.update(req.body, {
+    Review.update(req.body, {
         where: { id: id }
     })
         .then(num =>{
             if (num == 1) {
                 res.send({
-                    message: "El usuario fue actualizado exitosamente."
+                    message: "El review fue actualizado exitosamente."
                 });
             } else {
                 res.send({
-                    message: `No se pudo actualizar el usuario con el id=${id}`
+                    message: `No se pudo actualizar el review con el id=${id}`
                 });
             }
         })
         .catch(err =>{
             res.status(500).send({
                 message:
-                    err.message || "Error actualizando el usuario con el id =" + id
+                    err.message || "Error actualizando el review con el id =" + id
             })
         })
 };
 
 exports.delete = (req, res) =>{
-    const id = req.params.idUser;
+    const id = req.params.idReview;
 
-    Usuario.destroy( {
+    Review.destroy( {
         where: { id: id },
     })
         .then(num =>{
             if (num == 1) {
                 res.send({
-                    message: "El usuario fue eliminado exitosamente."
+                    message: "El review fue eliminado exitosamente."
                 });
             } else {
                 res.send({
-                    message: `No se pudo eliminar el usuario con el id=${id}`
+                    message: `No se pudo eliminar el review con el id=${id}`
                 });
             }
         })
         .catch(err =>{
             res.status(500).send({
                 message:
-                    err.message || "Error eliminando el usuario con el id =" + id
+                    err.message || "Error eliminando el review con el id =" + id
             })
         })
 
