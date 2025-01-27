b8c0e9fb3719c45d3b356a56d37066c8bf7499e1
EleArtee <100388234+EleArtee@users.noreply.github.com>
2025-01-27 02:12:02 +0000

    edit: Updated Controllers (usuario, perfil, edited juego)

--- backend/controllers/perfil.controller.js
+++ backend/controllers/perfil.controller.js
@@ -1,116 +1,105 @@
 const db = require("../models");
-const Juego = db.juegos;
+const Perfil = db.perfil;
 const Op = db.Sequelize.Op;
 
 exports.create = (req, res) =>{
-    const juego = {
-       nombre: req.body.nombre,
-       descripcion: req.body.descripcion,
-       precio: req.body.precio,
-       desarrolladora: req.body.desarrolladora,
-       filename: req.file ? req.file.filename : ""
+    const perfil = {
+       descripcion: ""
     }
 
-    if (!req.body.nombre){
-        res.status(400).send({
-            message: "Falta información obligatoria"
-        });
-        return;
-    }
-
-    Juego.create(juego)
+    Perfil.create(perfil)
     .then(data =>{
         res.send(data);
     })
     .catch(err => {
         res.status(500).send({
             message:
-                err.message || "Algo pasó mientras se creaba el juego de mesa."
+                err.message || "Algo pasó mientras se creaba el perfil."
         })
     });
 };
 
 exports.findAll = (req, res) =>{
-    Juego.findAll()
+    Perfil.findAll()
     .then(data => {
         res.send(data);
     })
     .catch(err =>{
         res.status(500).send({
             message:
-                err.message || "Algún error ocurrió mientras se obtenían los juegos de mesa."
+                err.message || "Algún error ocurrió mientras se obtenían los Perfils de mesa."
         })
     })
 
 };
 
 exports.findOne = (req, res) =>{
-    const id = req.params.idGame;
-    Juego.findByPK(id)
+    const id = req.params.idProfile;
+    Perfil.findByPK(id)
         .then(data =>{
             if (data) {
                 res.send(data);
             }
             else {
                 res.status(404).send({
-                    message: `No se pudo encontrar el juego con el id=${id}.`
+                    message: `No se pudo encontrar el perfil con el id=${id}.`
                 });
             }
         })
         .catch(err =>{
             res.status(500).send({
                 message:
-                    err.message || "Error obteniendo el juego con el id =" + id
+                    err.message || "Error obteniendo el perfil con el id =" + id
             })
         })
 };
 
 exports.update = (req, res) => {
-    const id = req.params.idGame;
+    const id = req.params.idProfile;
 
-    Juego.update(req.body, {
+    Perfil.update(req.body, {
         where: { id: id }
     })
         .then(num =>{
             if (num == 1) {
                 res.send({
-                    message: "El juego fue actualizado exitosamente."
+                    message: "El perfil fue actualizado exitosamente."
                 });
             } else {
                 res.send({
-                    message: `No se pudo actualizar el juego con el id=${id}`
+                    message: `No se pudo actualizar el perfil con el id=${id}`
                 });
             }
         })
         .catch(err =>{
             res.status(500).send({
                 message:
-                    err.message || "Error actualizando el juego con el id =" + id
+                    err.message || "Error actualizando el perfil con el id =" + id
             })
         })
 };
 
 exports.delete = (req, res) =>{
-    const id = req.params.idGame;
+    const id = req.params.idProfile;
 
-    Juego.destroy( {
+    Perfil.destroy( {
         where: { id: id },
     })
         .then(num =>{
             if (num == 1) {
                 res.send({
-                    message: "El juego fue eliminado exitosamente."
+                    message: "El perfil fue eliminado exitosamente."
                 });
             } else {
                 res.send({
-                    message: `No se pudo eliminar el juego con el id=${id}`
+                    message: `No se pudo eliminar el perfil con el id=${id}`
                 });
             }
         })
         .catch(err =>{
             res.status(500).send({
                 message:
-                    err.message || "Error eliminando el juego con el id =" + id
+                    err.message || "Error eliminando el perfil con el id =" + id
             })
         })
 
--- backend/controllers/usuario.controller.js
+++ backend/controllers/usuario.controller.js
@@ -1,116 +1,115 @@
 const db = require("../models");
-const Juego = db.juegos;
+const Usuario = db.usuarios;
 const Op = db.Sequelize.Op;
 
 exports.create = (req, res) =>{
-    const juego = {
-       nombre: req.body.nombre,
-       descripcion: req.body.descripcion,
-       precio: req.body.precio,
-       desarrolladora: req.body.desarrolladora,
-       filename: req.file ? req.file.filename : ""
+    const usuario = {
+       nick: req.body.nick,
+       email: req.body.email,
+       contraseña: req.body.contraseña,
+       empresa: req.body.empresa,
     }
 
-    if (!req.body.nombre){
+    if (!req.body.nick){
         res.status(400).send({
             message: "Falta información obligatoria"
         });
         return;
     }
 
-    Juego.create(juego)
+    Usuario.create(usuario)
     .then(data =>{
         res.send(data);
     })
     .catch(err => {
         res.status(500).send({
             message:
-                err.message || "Algo pasó mientras se creaba el juego de mesa."
+                err.message || "Algo pasó mientras se creaba el usuario de mesa."
         })
     });
 };
 
 exports.findAll = (req, res) =>{
-    Juego.findAll()
+    Usuario.findAll()
     .then(data => {
         res.send(data);
     })
     .catch(err =>{
         res.status(500).send({
             message:
-                err.message || "Algún error ocurrió mientras se obtenían los juegos de mesa."
+                err.message || "Algún error ocurrió mientras se obtenían los usuarios de mesa."
         })
     })
 
 };
 
 exports.findOne = (req, res) =>{
-    const id = req.params.idGame;
-    Juego.findByPK(id)
+    const id = req.params.idUser;
+    Usuario.findByPK(id)
         .then(data =>{
             if (data) {
                 res.send(data);
             }
             else {
                 res.status(404).send({
-                    message: `No se pudo encontrar el juego con el id=${id}.`
+                    message: `No se pudo encontrar el usuario con el id=${id}.`
                 });
             }
         })
         .catch(err =>{
             res.status(500).send({
                 message:
-                    err.message || "Error obteniendo el juego con el id =" + id
+                    err.message || "Error obteniendo el usuario con el id =" + id
             })
         })
 };
 
 exports.update = (req, res) => {
-    const id = req.params.idGame;
+    const id = req.params.idUser;
 
-    Juego.update(req.body, {
+    Usuario.update(req.body, {
         where: { id: id }
     })
         .then(num =>{
             if (num == 1) {
                 res.send({
-                    message: "El juego fue actualizado exitosamente."
+                    message: "El usuario fue actualizado exitosamente."
                 });
             } else {
                 res.send({
-                    message: `No se pudo actualizar el juego con el id=${id}`
+                    message: `No se pudo actualizar el usuario con el id=${id}`
                 });
             }
         })
         .catch(err =>{
             res.status(500).send({
                 message:
-                    err.message || "Error actualizando el juego con el id =" + id
+                    err.message || "Error actualizando el usuario con el id =" + id
             })
         })
 };
 
 exports.delete = (req, res) =>{
-    const id = req.params.idGame;
+    const id = req.params.idUser;
 
-    Juego.destroy( {
+    Usuario.destroy( {
         where: { id: id },
     })
         .then(num =>{
             if (num == 1) {
                 res.send({
-                    message: "El juego fue eliminado exitosamente."
+                    message: "El usuario fue eliminado exitosamente."
                 });
             } else {
                 res.send({
-                    message: `No se pudo eliminar el juego con el id=${id}`
+                    message: `No se pudo eliminar el usuario con el id=${id}`
                 });
             }
         })
         .catch(err =>{
             res.status(500).send({
                 message:
-                    err.message || "Error eliminando el juego con el id =" + id
+                    err.message || "Error eliminando el usuario con el id =" + id
             })
         })
 
