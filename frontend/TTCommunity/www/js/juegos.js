8bf34cf8e96da3872f8d51361216c28be6a030fc
EleArtee <100388234+EleArtee@users.noreply.github.com>
2025-02-26 16:43:40 +0000

    edit: media de reviews

--- frontend/TTCommunity/www/js/juegos.js
+++ frontend/TTCommunity/www/js/juegos.js
@@ -1,5 +1,6 @@
 const API_URL = 'http://localhost:8080/api/juego'
 let TAG_URL = 'http://localhost:8080/api/tag/'
+const REVIEW_URL = 'http://localhost:8080/api/review'
 
 async function desconectar(){
   localStorage.removeItem('token');
@@ -52,19 +53,46 @@ async function checkLogging(){
 
   async function allGames() {
     const response = await fetch(API_URL, {method: 'GET'})
-    
     let data = await response.json()
+
+    const resp = await fetch(REVIEW_URL, {method: 'GET'})
+    let redata = await resp.json()
 
     let placeholder = document.querySelector("#juegos");
     let out = "";
+
+    let stars = 0;
+    let nReviews = 0;
+
+   
     for (let game of data){
+      for (let review of redata){
+        if (review.idGame == game.idGame){
+            stars += review.estrellas
+            nReviews += 1
+            console.log(stars)
+        }
+    }
+
+    let media = 0;
+
+    if (stars == 0){
+        media = 0
+    }
+    else{
+        media = stars/nReviews
+    }
+
       out +=`
         <a href="juego.html?id=${game.idGame}" class="gamelink"><div class="juego">
           <img class="imgjuego" src="${game.filename}" referrerpolicy="no-referrer" alt="">
           <h3 class="text">${game.nombre}</h3>
-          <p class="text">media</p>
+          <p class="text">${media}</p>
         </div></a>
       `;
+      media = 0;
+      stars = 0;
+      nReviews = 0;
     }
 
     placeholder.innerHTML = out;
